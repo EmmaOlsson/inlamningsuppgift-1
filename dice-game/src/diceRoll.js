f6febacbdd9569c31f4471860ec21c9a8d82ea47
Emma Olsson <emma.olsson22@learnet.se>
2021-01-12 11:05:03 +0100

    Deleted dice
    Deleted unused dice

--- dice-game/src/diceRoll.js
+++ dice-game/src/diceRoll.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import DiceDots from './diceDots';
-
 
 function DiceRoll(props) {
     return (
             <section className="userButtons">
                 <button disabled={ props.dieOneRolled } onClick={props.rollDieOne}><i className="fas fa-question"></i>
-                <DiceDots />
                 </button>
                 <button  disabled={ props.dieTwoRolled } onClick={props.rollDieTwo}><i className="fas fa-question"></i>
-                <DiceDots />
                 </button>
                 <button  disabled={ props.dieThreeRolled } onClick={props.rollDieThree}><i className="fas fa-question"></i>
-                <DiceDots />
                 </button>
             </section>
     )
